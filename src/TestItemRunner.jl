9660cb1c26132035d93ec998d83c363541c51053
davidanthoff <davidanthoff@users.noreply.github.com>
2022-08-15 22:15:15 +0000

    Format files using DocumentFormat

--- src/TestItemRunner.jl
+++ src/TestItemRunner.jl
@@ -14,7 +14,7 @@ function compute_line_column(content, target_pos)
 
     pos = 1
     while pos < target_pos
-        if content[pos]=='\n'
+        if content[pos] == '\n'
             line += 1
             column = 1
         else
@@ -32,30 +32,30 @@ end
 
     content = "abc\ndef\nghi"
 
-    @test TestItemRunner.compute_line_column(content, 1) == (1,1)
-    @test TestItemRunner.compute_line_column(content, 2) == (1,2)
-    @test TestItemRunner.compute_line_column(content, 3) == (1,3)
-    @test TestItemRunner.compute_line_column(content, 5) == (2,1)
-    @test TestItemRunner.compute_line_column(content, 6) == (2,2)
-    @test TestItemRunner.compute_line_column(content, 7) == (2,3)
-    @test TestItemRunner.compute_line_column(content, 9) == (3,1)
-    @test TestItemRunner.compute_line_column(content, 10) == (3,2)
-    @test TestItemRunner.compute_line_column(content, 11) == (3,3)
+    @test TestItemRunner.compute_line_column(content, 1) == (1, 1)
+    @test TestItemRunner.compute_line_column(content, 2) == (1, 2)
+    @test TestItemRunner.compute_line_column(content, 3) == (1, 3)
+    @test TestItemRunner.compute_line_column(content, 5) == (2, 1)
+    @test TestItemRunner.compute_line_column(content, 6) == (2, 2)
+    @test TestItemRunner.compute_line_column(content, 7) == (2, 3)
+    @test TestItemRunner.compute_line_column(content, 9) == (3, 1)
+    @test TestItemRunner.compute_line_column(content, 10) == (3, 2)
+    @test TestItemRunner.compute_line_column(content, 11) == (3, 3)
 end
 
 function find_test_items_detail!(filename, content, node, testitems)
     node isa EXPR || return
 
-    if node.head == :macrocall && length(node.args)==4 && CSTParser.valof(node.args[1]) == "@testitem"
+    if node.head == :macrocall && length(node.args) == 4 && CSTParser.valof(node.args[1]) == "@testitem"
 
         pos = get_file_loc(node.args[4])[2]
 
         loc = pos:pos+node.args[4].span
-    
+
         line, column = compute_line_column(content, pos)
 
-        push!(testitems, (filename=filename, code=content[loc], name=CSTParser.valof(node.args[3]), line=line-1, column=column))
-    elseif node.head == :module && length(node.args)>=3 && node.args[3] isa EXPR && node.args[3].head==:block
+        push!(testitems, (filename=filename, code=content[loc], name=CSTParser.valof(node.args[3]), line=line - 1, column=column))
+    elseif node.head == :module && length(node.args) >= 3 && node.args[3] isa EXPR && node.args[3].head == :block
         for i in node.args[3].args
             find_test_items_detail!(filename, content, i, testitems)
         end
@@ -68,7 +68,7 @@ function run_testitem(filepath, original_code, line, column)
     code_without_begin_end = strip(original_code)[6:end-3]
     code = string('\n'^line, ' '^column, code_without_begin_end)
 
-    
+
 
     withpath(filepath) do
         Base.invokelatest(include_string, mod, code, filepath)
@@ -92,13 +92,13 @@ function run_tests(path)
     for file in julia_files
         content = read(file, String)
         cst = CSTParser.parse(content, true)
-        
+
         testitems_for_file = []
         for i in cst.args
             find_test_items_detail!(file, content, i, testitems_for_file)
         end
 
-        if length(testitems_for_file)>0
+        if length(testitems_for_file) > 0
             testitems[file] = testitems_for_file
         end
     end
