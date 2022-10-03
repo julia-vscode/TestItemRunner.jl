1cbf2a70eaa14a25ce681cbbf7ad001f2d33b4ab
David Anthoff <anthoff@berkeley.edu>
2022-10-03 13:04:00 -0700

    Fix CSTParser imports

--- src/TestItemRunner.jl
+++ src/TestItemRunner.jl
@@ -6,7 +6,7 @@ module CSTParser
     using ..Tokenize
     import ..Tokenize.Tokens
     import ..Tokenize.Tokens: RawToken, AbstractToken, iskeyword, isliteral, isoperator, untokenize
-    import ..Tokenize.Lexers: Lexer, peekchar, iswhitespace
+    import ..Tokenize.Lexers: Lexer, peekchar, iswhitespace, readchar, emit, emit_error,  accept_batch, eof
 
     include("../packages/CSTParser/src/packagedef.jl")
 end
